9ca6d2e025c5a29b96fd2c0bff2570524038c081
kane5 <Nguyenkhactam5@gmail.com>
2025-02-06 04:23:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import environ
 
 API_HASH = environ.get("API_HASH", "146fcf83a1670e2e926ce874ce31fcb3")
 API_ID = int(environ.get("API_ID", "29762406"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7852033838:AAEjvbXsgvFQHrhZOIz3TEjWqsKr_9MFknI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8152803267:AAF1RHR_Olv8Ze-mA6E0DQu9GwAaoMHCGcE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "6670233911"))
-BOT_USERNAME = environ.get("BOT_USERNAME", "utmapbot")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Choikhomaubot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002465408399"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002271972643"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://nguyenkhactam5:q1231234@kanereaction.zhqqd.mongodb.net/?retryWrites=true&w=majority&appName=kanereaction")
